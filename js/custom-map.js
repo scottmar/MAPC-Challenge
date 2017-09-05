84365a691b8b397eda9fe1082f95bd4451d2b5f1
Scott <scottmar@mit.edu>
2017-09-05 14:26:19 -0400

    initial map set-up

--- js/custom-map.js
+++ js/custom-map.js
@@ -1,4 +1,4 @@
-var map = L.map('map',{center: [42.358733, -71.107572], zoom:16, scrollWheelZoom:false});
+var map = L.map('map',{center: [42.335174, -71.078368], zoom:12, scrollWheelZoom:false});
 		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
